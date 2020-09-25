61b059a6ee07e6e4557ebc831cb1320bbbf7a363
Pankaj Kumar <2016mepankaj4882@gmail.com>
2020-09-25 13:22:15 +0530

    updated client and server

---
+++ client2.py
@@ -0,0 +1,35 @@
+from Requirements.library import *
+  
+class Client:
+    def __init__(self, host='127.0.0.1', port=1234):
+        self.client = self.Socket(host, port)
+        message = threading.Thread(target = self.Message)
+        receive = threading.Thread(target = self.Receive)
+        message.start()
+        receive.start()
+
+    def Socket(self,host, port):
+	    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	    client.connect((host,port))
+	    return client
+
+    def Receive(self, *args):
+        try:
+          while True:
+              msg = self.client.recv(1024)
+              if len(msg)!=0:
+                  print(msg)
+                    
+        except:
+            pass
+          
+          
+    def Message(self, *args):
+        while True:
+            msg='client2-'
+            msg += input()
+            msg = msg.encode('utf-8')
+            self.client.send(msg)
+
+if __name__ == '__main__':
+	app = Client()
--- server.py
+++ server.py
@@ -3,10 +3,11 @@ from Requirements.library import *
 class Server:
     def __init__(self, host='127.0.0.1', port=1234):
         self.server = self.Socket(host, port)
-        self.client = []
+        self.client = [self.server]
 
     def Socket(self,host, port):
 	    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	    server.bind((host,port))
 	    server.listen()
 	    return server
@@ -19,16 +20,17 @@ class Server:
                   if client == self.server:
                       client_socket, address = client.accept()
                       self.client.append(client_socket)
+                      print(f'successfully connected with {address[0]}')
                       Message = b'Welcome'
-                      self.Send(client_socket, Message) 
+                      client_socket.send(Message)
 
                   else:
                       Message = self.Receive(client)
                       if len(Message)!=0:
                           self.Send(client, Message)
                       
-             for client in disconnect_socket:
-                  del client
+             for client in disconnected_socket:
+                  client.close()
                   self.client.remove(client)
                   
                   
@@ -36,7 +38,7 @@ class Server:
         try:
           while True:
               msg = client.recv(1024)
-              if len(msg)==0:
+              if len(msg)!=0:
                   return msg
                     
         except:
@@ -44,6 +46,13 @@ class Server:
           
           
     def Send(self, client, message):
-        client.sendall(message)
+        for i in self.client[1:]:
+            if i!=client:
+                i.send(message)
+
+                
+if __name__ == '__main__':
+	app = Server()
+	app.Accept()
               
  
